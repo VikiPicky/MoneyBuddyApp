cb67ac036b4750f1c7062b610becb4b0afcc3875
VikiPicky <v.mitiakova@gmail.com>
2022-06-24 20:03:23 -0400

    ActivateAccountServlet
    ActivateAccountServlet works, changes 0 to 1 in Active USER

--- src/main/java/com/registration/controller/ActivateAccount.java
+++ src/main/java/com/registration/controller/ActivateAccount.java
@@ -1,59 +1,69 @@
 package com.registration.controller;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import com.registration.DAO.ConnectionDB;
 
 @WebServlet("/ActivateAccount")
 public class ActivateAccount extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 
-    public ActivateAccount() {
-        super();
-        
-    }
+	public ActivateAccount() {
+		super();
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		String userEmail = request.getParameter("key");
-		
-		Connection con = ConnectionDB.getConnection();
-		
-		System.out.println("From Activate Account - CONNECTED");
+
 		try {
-			PreparedStatement pst = con.prepareStatement("SELECT email, active FROM user WHERE email=" + userEmail + " AND active='0'");
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("ActivateAccount: " + e);
+		}
+		String userEmail = request.getParameter("key");
+		Connection con = ConnectionDB.getConnection();
+
+		System.out.println("ActivateAccount: Connected");
+
+		try {
+			PreparedStatement pst = con.prepareStatement("SELECT email, active FROM user WHERE email='" + userEmail + "' AND active='0';");
 			ResultSet rs = pst.executeQuery();
-			
+
+
 			if (rs.next()) {
-				PreparedStatement update = con.prepareStatement("UPDATE user SET active='1' WHERE email=" + userEmail);
+				PreparedStatement update = con
+						.prepareStatement("UPDATE user SET active='1' WHERE email='" + userEmail + "';");
+				
 				int i = update.executeUpdate();
-				if (i == 1) {
+				System.out.println("Activate account " + i);
+				if (i > 0) {
+					
 					response.sendRedirect("SignIn.html");
 				} else {
 					response.sendRedirect("index.html");
 				}
+			} else {
+				System.out.println("Activate account : No suitable records");
 			}
 		} catch (Exception ex) {
-			
-		}	
-		
-		
+			System.out.println("ActivateAccount:4" + ex.getMessage());
+		}
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		doGet(request, response);
 	}
 
--- src/main/java/com/registration/controller/SignInServlet.java
+++ src/main/java/com/registration/controller/SignInServlet.java
@@ -1,9 +1,7 @@
 package com.registration.controller;
 
+
 import java.io.IOException;
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -17,38 +15,16 @@ public class SignInServlet extends HttpServlet {
        
     public SignInServlet() {
         super();
-    
-    }
+        }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String userNameProvided = request.getParameter("userName");
-		String passwordProvided = request.getParameter("password");
+		doGet(request, response);
 		
-		String hashedPwd;
-		try {
-			hashedPwd = createHash(passwordProvided);
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-			throw new ServletException("Извините, всё сломалось.!");
-		}
-
 	}
-	private static String createHash(String passwordProvided) throws NoSuchAlgorithmException {
 
-		MessageDigest md = MessageDigest.getInstance("MD5");
-		byte[] messageDigest = md.digest(passwordProvided.getBytes());
-		BigInteger no = new BigInteger(1, messageDigest);
-
-		String hashedPWDCompare = no.toString(16);
-		while (hashedPWDCompare.length() < 32) {
-			hashedPWDCompare = "0" + hashedPWDCompare;
-		}
-		return hashedPWDCompare; //// compare to DB insance in the DB
-
-	}
 }
